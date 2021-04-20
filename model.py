e24574ec33929f1f3b3abb14e3a472a296416fe5
Jan Kamnikar <jann.kamnikar@gmail.com>
2021-04-20 15:42:48 +0200

    Dodali začetne datoteke

---
+++ model.py
@@ -0,0 +1,47 @@
+STEVILO_DOVOLJENIH_NAPAK = 10
+PRAVILNA_CRKA, PONOVLJENA_CRKA, NAPACNA_CRKA = '+', 'o', '-'
+ZMAGA, PORAZ = 'W', 'X'
+
+class Igra:
+    def __init__(self, geslo, crke=[]): #geslo=niz crke=seznam
+        self.geslo = geslo
+        self.crke = crke
+
+    def napacne_crke(self):
+       return [c for c in self.crke if c.upper() not in self.geslo.upper()]
+    def pravilne_crke(self):
+        return [c for c in self.crke if c.upper() in self.geslo.upper()]
+    def stevilo_napak(self):
+        return len(self.napacne_crke())
+    def poraz(self):
+        return self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK    
+    def zmaga(self):
+        return not self.poraz() and len(self.pravilne_crke()) == len(set(self.geslo))
+        #set()ti da množico, ker množica nima ponovljenih črk npr žaba pravilne crke žba, 3!=4
+    def pravilni_del_gesla(self):
+        return ''.join([crka if crka in self.crke else '_' for crka in self.geslo.upper()])#c self.crke dajemo samo velike crke drugače pa samo str(self.crke).upper()
+    def nepravilni_ugibi(self):
+        return ' '.join(self.napacne_crke())#join ti spremeni v niz
+    def ugibaj(self,crka):
+        crka = crka.upper()
+        if crka in self.crke:
+            return PONOVLJENA_CRKA
+        elif crka in self.geslo.upper():
+            self.crke.append(crka)
+            if self.zmaga():
+                return ZMAGA
+            else:
+                return PRAVILNA_CRKA
+        else:
+            self.crke.append(crka)
+            if self.poraz():
+                return PORAZ
+            else:
+                return NAPACNA_CRKA
+with open('besede.txt', encoding='utf-8') as f:
+    bazen_besed = f.read().split() #read spremeni v niz, split pa v seznam
+
+import random
+def nova_igra():
+    geslo = random.choice(bazen_besed)
+    return Igra(geslo)
